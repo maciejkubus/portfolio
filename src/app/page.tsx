071c8cd17984da300c300274d30607d1d2e5ae7b
Julia Dmytrenko <dmytrenkojulia@gmail.com>
2024-06-23 15:35:35 +0200

    add flowbite and navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { Nav } from "@/components/Nav";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Portfolio
+    <main>
+      <Nav />
     </main>
   );
 }
