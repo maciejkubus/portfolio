f97317d61cf9e70b0b886fec7fd3ef71e1baea22
Maciej Kubus <maciek.krol.zycia@gmail.com>
2024-06-23 17:01:24 +0200

    fix

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -21,20 +21,6 @@ export function Nav() {
       </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse>
-        <Navbar.Link href="#" active>
-          Strona główna
-        </Navbar.Link>
-        <Navbar.Link as={Link} href="#">
-          Portfolio
-        </Navbar.Link>
-        <Navbar.Link href="#">Zespół</Navbar.Link>
-        <Navbar.Link href="#">Blog</Navbar.Link>
-        <Navbar.Link href="#">Kontakt</Navbar.Link>
-        <Navbar.Link href="#">
-          <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded-full">
-            Bezpłatna wycena
-          </button>
-        </Navbar.Link>
         <Navbar.Link href="#">Services</Navbar.Link>
         <Navbar.Link href="#">Pricing</Navbar.Link>
         <Navbar.Link href="#">Contact</Navbar.Link>
