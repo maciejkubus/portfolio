81eb4b6e8359d7a14bfa3458a424e2903a82dcd3
Julia Dmytrenko <dmytrenkojulia@gmail.com>
2024-06-23 16:52:30 +0200

    fix button in navigation

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,10 +2,18 @@
 
 import Link from "next/link";
 import { Navbar } from "flowbite-react";
+import { Button } from "flowbite-react";
+import type { CustomFlowbiteTheme } from "flowbite-react";
+
+const customTheme: CustomFlowbiteTheme["navbar"] = {
+  collapse: {
+    list: "mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium items-center",
+  },
+};
 
 export function Nav() {
   return (
-    <Navbar fluid rounded>
+    <Navbar fluid rounded theme={customTheme}>
       <Navbar.Brand as={Link} href="https://flowbite-react.com">
         <span className="self-center whitespace-nowrap text-xl font-semibold text-black">
           Flowbite React
@@ -22,6 +30,11 @@ export function Nav() {
         <Navbar.Link href="#">Services</Navbar.Link>
         <Navbar.Link href="#">Pricing</Navbar.Link>
         <Navbar.Link href="#">Contact</Navbar.Link>
+        <Navbar.Link href="#">
+          <Button color="light" pill>
+            Light
+          </Button>
+        </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
   );
