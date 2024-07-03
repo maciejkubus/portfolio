19148277783e80ec04ced26ee138b5b1d22bf385
Julia Dmytrenko <dmytrenkojulia@gmail.com>
2024-07-03 17:27:49 +0200

    add react-icons and ContactUs section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import ArticleTile from "@/components/ArticleTile";
+import ContactUsSection from "@/components/ContactUsSection";
 import PersonTile from "@/components/PersonTile";
 import ProjectTile from "@/components/ProjectTile";
 import { ServiceTile } from "@/components/ServiceTile";
@@ -10,20 +11,32 @@ export default function Home() {
       <div className="py-32 w-full flex gap-8">
         <div className="w-1/2">
           <h1 className="font-bold text-6xl leading-normal">
-            Zbudujemy razem<br /> 
-            twoje miejsce<br />
-            w internecie
+            Zbudujemy razem
+            <br />
+            twoje miejsce
+            <br />w internecie
           </h1>
           <p className="text-xl my-8">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis cupiditate facere sequi hic exercitationem nisi rem, tempora accusamus aliquid, mollitia suscipit minima neque.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+            cupiditate facere sequi hic exercitationem nisi rem, tempora
+            accusamus aliquid, mollitia suscipit minima neque.
           </p>
           <div className="flex gap-8">
-            <button className="bg-primary rounded-xl font-bold px-6 py-3 text-white">Skontaktuj się z nami</button>
-            <button className="border-black border-2 rounded-xl px-6 py-3 font-bold">Zobacz nasze prace</button>
+            <button className="bg-primary rounded-xl font-bold px-6 py-3 text-white">
+              Skontaktuj się z nami
+            </button>
+            <button className="border-black border-2 rounded-xl px-6 py-3 font-bold">
+              Zobacz nasze prace
+            </button>
           </div>
         </div>
-        <div className="w-1/2"> 
-          <Image src="computer.svg" alt="computer" width={591} height={451}></Image>
+        <div className="w-1/2">
+          <Image
+            src="computer.svg"
+            alt="computer"
+            width={591}
+            height={451}
+          ></Image>
         </div>
       </div>
       <div className="py-32 w-full flex gap-8">
@@ -40,57 +53,66 @@ export default function Home() {
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis cupiditate facere sequi hic exercitationem nisi rem, tempora accusamus aliquid, mollitia suscipit minima neque."
         ></ServiceTile>
       </div>
-      <div className="w-full flex gap-8 font-bold text-6xl">Kilka naszych projektów</div>
+      <div className="w-full flex gap-8 font-bold text-6xl">
+        Kilka naszych projektów
+      </div>
       <div className="pt-8 pb-32 w-full grid grid-cols-2 gap-8">
-        <ProjectTile 
+        <ProjectTile
           title="Projekt #1"
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis cupiditate facere sequi hic exercitationem nisi rem, tempora accusamus aliquid, mollitia suscipit minima neque"
         ></ProjectTile>
-        <ProjectTile 
+        <ProjectTile
           title="Projekt #2"
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis cupiditate facere sequi hic exercitationem nisi rem, tempora accusamus aliquid, mollitia suscipit minima neque"
         ></ProjectTile>
-        <ProjectTile 
+        <ProjectTile
           title="Projekt #3"
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis cupiditate facere sequi hic exercitationem nisi rem, tempora accusamus aliquid, mollitia suscipit minima neque"
         ></ProjectTile>
-        <ProjectTile 
+        <ProjectTile
           title="Projekt #4"
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis cupiditate facere sequi hic exercitationem nisi rem, tempora accusamus aliquid, mollitia suscipit minima neque"
         ></ProjectTile>
       </div>
       <div className="flex justify-center pb-16">
-        <button className="bg-primary rounded-xl font-bold px-8 py-4 text-white text-2xl">Zobacz wszystkie projekty</button>
+        <button className="bg-primary rounded-xl font-bold px-8 py-4 text-white text-2xl">
+          Zobacz wszystkie projekty
+        </button>
       </div>
-      <div className="w-full flex gap-8 font-bold text-6xl">Nasz wyjątkowy zespół</div>
+      <div className="w-full flex gap-8 font-bold text-6xl">
+        Nasz wyjątkowy zespół
+      </div>
       <div className="pb-32 pt-8 w-full flex gap-7 my-7">
         <PersonTile
           title="Astoria Bublas"
-          text="Lorem ipsum dolor sit amet facere et consectetur adipisicing elit. Quisis cupiditate facere seq exercitationem.">
-        </PersonTile>
+          text="Lorem ipsum dolor sit amet facere et consectetur adipisicing elit. Quisis cupiditate facere seq exercitationem."
+        ></PersonTile>
         <PersonTile
           title="Maciej Kubus"
-          text="Lorem ipsum dolor sit amet facere et consectetur adipisicing elit. Quisis cupiditate facere seq exercitationem.">
-        </PersonTile>
+          text="Lorem ipsum dolor sit amet facere et consectetur adipisicing elit. Quisis cupiditate facere seq exercitationem."
+        ></PersonTile>
         <PersonTile
           title="Julia Dmytrenko"
-          text="Lorem ipsum dolor sit amet facere et consectetur adipisicing elit. Quisis cupiditate facere seq exercitationem.">
-        </PersonTile>
+          text="Lorem ipsum dolor sit amet facere et consectetur adipisicing elit. Quisis cupiditate facere seq exercitationem."
+        ></PersonTile>
       </div>
       <div className="w-full flex gap-8 font-bold text-6xl">Aktualności</div>
       <div className="pt-8 pb-32 w-full grid grid-cols-2 gap-8">
-        <ArticleTile 
+        <ArticleTile
           title="Artykuł #1"
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis cupiditate facere sequi hic exercitationem nisi rem, tempora accusamus aliquid, mollitia suscipit minima neque"
         ></ArticleTile>
-        <ArticleTile 
+        <ArticleTile
           title="Artykuł #2"
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis cupiditate facere sequi hic exercitationem nisi rem, tempora accusamus aliquid, mollitia suscipit minima neque"
         ></ArticleTile>
       </div>
       <div className="flex justify-center pb-16">
-        <button className="bg-primary rounded-xl font-bold px-8 py-4 text-white text-2xl">Przejdź do naszego bloga</button>
+        <button className="bg-primary rounded-xl font-bold px-8 py-4 text-white text-2xl">
+          Przejdź do naszego bloga
+        </button>
       </div>
+      <ContactUsSection />
     </main>
   );
 }
---
+++ src/components/ContactUsSection.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { CiLinkedin } from "react-icons/ci";
+
+const ContactUsSection = () => {
+  return (
+    <div>
+      <div className="pt-8 pb-32 w-full grid grid-cols-2 gap-8">
+        <div>
+          <h3 className="w-full flex gap-8 font-bold text-6xl mb-8">
+            Odezwij się do nas
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+            elementum, felis ac elementum efficitur, sapien tellus eleifend
+            enim, quis consequat lorem metus nec tortor. Morbi eu sem in quam
+            cursus sodales. Donec commodo felis ac elit porta malesuada. Aenean
+            vel dignissim erat, vitae auctor quam.
+          </p>
+          <div className="pt-8 pb-32 w-full grid grid-cols-2 gap-2">
+            <div className="flex items-center">
+              <CiLinkedin className="inline h-9 w-9 mr-1" />
+              <span>LinkedIn</span>
+            </div>
+            <div className="flex items-center">
+              <CiLinkedin className="inline h-9 w-9 mr-1" />
+              <span>LinkedIn</span>
+            </div>
+          </div>
+        </div>
+        <div>formularz</div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUsSection;
