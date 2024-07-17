13e8390a889e963d043753abd7b6ce9eb2fd9725
Maciej Kubus <maciek.krol.zycia@gmail.com>
2024-07-17 10:41:28 +0200

    fix popup size

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -16,7 +16,7 @@ export function Popup({
 			return (<></>)
 		} else {
 			return (
-				<div className="inset-0 fixed bg-opacity-80 bg-black flex justify-center items-center" onClick={() => togglePopup()}>
+				<div className="inset-0 fixed bg-opacity-80 bg-black flex justify-center items-center p-0 md:p-8" onClick={() => togglePopup()}>
 					<div className="container p-8 rounded-xl bg-white" onClick={e => e.stopPropagation()}>
 						<div className="text-2xl flex justify-end">
 							<FaTimes onClick={() => togglePopup()} className="cursor-pointer hover:rotate-90 transition-all duration-300"/>
