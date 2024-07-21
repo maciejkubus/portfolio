4673c3ca6e81ed9f1ebe775116c4907b49ee8fc6
Maciej Kubus <maciek.krol.zycia@gmail.com>
2024-07-21 21:45:40 +0200

    +mail, phone

--- src/components/ContactUsSection.tsx
+++ src/components/ContactUsSection.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { CiLinkedin } from 'react-icons/ci'
+import { CiLinkedin, CiMail, CiPhone } from 'react-icons/ci'
 import ContactForm from './ContactForm'
 
 const ContactUsSection = () => {
@@ -13,7 +13,15 @@ const ContactUsSection = () => {
                     <p>
 											Chętnie odpowiemy na wszystkie Twoje pytania i wątpliwości. Skontaktuj się z nami, korzystając z formularza obok lub bezpośrednio wysyłając e-mail na nasz adres. Odpowiemy na Twoją wiadomość najszybciej jak to możliwe.
                     </p>
-                    <div className="grid w-full grid-cols-2 gap-2 pb-32 pt-8">
+                    <div className="grid w-full grid-cols-2 gap-4 pb-32 pt-8">
+												<div className="flex items-center">
+                            <CiMail className="mr-1 inline h-9 w-9" />
+                            <a href="mailto:astoriastar@email.com">astoriastar@email.com</a>
+                        </div>
+												<div className="flex items-center">
+                            <CiPhone className="mr-1 inline h-9 w-9" />
+                            <a href="tel:+48 570 277 237">+48 570 277 237</a>
+                        </div>
                         <div className="flex items-center">
                             <CiLinkedin className="mr-1 inline h-9 w-9" />
                             <span>LinkedIn</span>
